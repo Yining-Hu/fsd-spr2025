da2d0e7e8deeb18aee60f1cddf2f7d65100479cc
Yining Hu <yining.hu@uts.edu.au>
2025-09-30 15:57:27 +1000

    week10 sample solutions

---
+++ week10/GUIBankAppSkeleton/controller.py
@@ -0,0 +1,27 @@
+from model import Database
+
+# Controller class is completed
+class Controller:
+    def __init__(self):
+        self.model = Database()
+
+    def match(self,email,password):
+        return self.model.match(email,password)
+
+    @staticmethod
+    def save(user):
+        handler = open("./log.txt","a+")
+        handler.write(str(user))
+        handler.write("\n")
+        handler.close()
+
+    @staticmethod
+    def read():
+        handler = open("./log.txt","r")
+        data = handler.readlines()
+        lines = []
+        for line in data:
+            if not(line.isspace()):
+                filtered = line.replace("\n","")
+                lines.append(filtered)
+        return lines
